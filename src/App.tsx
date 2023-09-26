1c67087c8fb22d581a9edb3f954958ce43b1c42e
TTiploka <hcobasmatos@gmail.com>
2023-09-26 19:44:04 -0400

    init comit

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+
+function App() {
+  return (
+    <>
+
+  <section className='overflow-x-hidden'>
+  <div className='bg-stone-900 h-screen w-screen grid grid-cols-4 gap-2 p-20'> 
+  <button className='bg-blue-400 rounded-md p-2 text-white' >1</button>
+  <button className='bg-blue-400 rounded-md p-2 text-white' >2</button>
+  <button className='bg-blue-400 rounded-md p-2 text-white' >3</button>
+  <button className='bg-blue-400 rounded-md p-2 text-white' >4</button>
+  <button className='bg-blue-400 rounded-md p-2 text-white' >5</button>
+  <button className='bg-blue-400 rounded-md p-2 text-white' >6</button>
+  <button className='bg-blue-400 rounded-md p-2 text-white' >7</button>
+  <button className='bg-blue-400 rounded-md p-2 text-white' >8</button>
+  <button className='bg-blue-400 rounded-md p-2 text-white' >9</button>
+  <button className='bg-blue-400 rounded-md p-2 text-white' >0</button>
+  <button className='bg-blue-400 rounded-md p-2 text-white' >+</button>
+  <button className='bg-blue-400 rounded-md p-2 text-white' >-</button>
+  <button className='bg-blue-400 rounded-md p-2 text-white' >*</button>
+  <button className='bg-blue-400 rounded-md p-2 text-white' >/</button>
+  <button className='bg-blue-400 rounded-md p-2 col-span-2 text-white' >=</button>
+  </div>
+  <div className='bg-stone-900 h-screen w-screen grid grid-cols-3 gap-5'> 
+  <button className='bg-blue-400 rounded-md p-2 text-white' ></button>
+  <button className='bg-blue-400 rounded-md p-2 text-white' ></button>
+  <button className='bg-blue-400 rounded-md p-2 text-white' >8</button>
+  <button className='bg-blue-400 rounded-md p-2 text-white' >9</button>
+  </div>
+  <div className='bg-stone-900 h-screen w-screen columns-3 gap-5'> 
+  <button className='bg-blue-400 rounded-md p-2 text-white' >0</button>
+  <button className='bg-blue-400 insolate rounded-md p-2 text-white' >+</button>
+  <button className='bg-blue-400 rounded-md p-2 text-white' >-</button>
+  <button className='bg-blue-400 rounded-md p-2 text-white' >*</button>
+  <button className='bg-blue-400 rounded-md p-2 text-white' >/</button>
+  </div>
+  <div className="bg-stone-900 h-screen w-screen text-white columns-2">
+    <p className="insolate hover:break-after-column">Well, let me tell you something, ...</p>
+    <p className='insolate'>Sure, go ahead, laugh...</p>
+    <p>Maybe we can live without...</p>
+    <p>Look. If you think this is...</p>
+  </div>
+
+  </section>
+    </>
+  )
+}
+export default App
