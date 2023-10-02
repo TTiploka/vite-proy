b95e696e49c006dcf004ef98e7c42a5d76ab074a
Dricc-new <diegoramoncc17@gmail.com>
2023-10-02 19:38:39 -0400

    btn modified

--- src/App.tsx
+++ src/App.tsx
@@ -12,35 +12,35 @@ function App() {
             <input type="text" className="border rounded-md p-4 w-full bg-stone-900 text-white m-0 bottom-2  text-4xl" />
 
             <div className="flex flex-row gap-2 ">
-              <Btn text="PI"></Btn>
-              <Btn text="e"></Btn>
-              <Btn text="|n|"></Btn>
-              <Btn text="C"></Btn>
+              <Btn class='basis-1/5' text="PI"></Btn>
+              <Btn class='basis-1/5' text="e"></Btn>
+              <Btn class='basis-1/5' text="|n|"></Btn>
+              <Btn class='basis-1/5' text="C"></Btn>
               <button className="basis-1/5 rounded-md bg-slate-600 shadow-lg hover:shadow-cyan-500/50	text-white"><button className="bg-slate-950 w-1/3  border border-white rounded rounded-l-full ">X</button></button>
             </div>
             <div className="grid grid-cols-5 gap-2 row-span-4">
               <div className="rounded-md bg-stone-900 col-span-3 grid grid-cols-3 gap-2 ">
-                <button className="rounded-md bg-gradient-to-br from-blue-400 from-10% to-indigo-800 to-90% shadow-lg hover:shadow-cyan-500/50 ">1</button>
-                <button className="rounded-md bg-gradient-to-br from-blue-400 from-10% to-indigo-800 to-90% shadow-lg hover:shadow-cyan-500/50 ">2</button>
-                <button className="rounded-md bg-gradient-to-br from-blue-400 from-10% to-indigo-800 to-90% shadow-lg hover:shadow-cyan-500/50 ">3</button>
-                <button className="rounded-md bg-gradient-to-br from-blue-400 from-10% to-indigo-800 to-90% shadow-lg hover:shadow-cyan-500/50 ">4</button>
-                <button className="rounded-md bg-gradient-to-br from-blue-400 from-10% to-indigo-800 to-90% shadow-lg hover:shadow-cyan-500/50 ">5</button>
-                <button className="rounded-md bg-gradient-to-br from-blue-400 from-10% to-indigo-800 to-90% shadow-lg hover:shadow-cyan-500/50 ">6</button>
-                <button className="rounded-md bg-gradient-to-br from-blue-400 from-10% to-indigo-800 to-90% shadow-lg hover:shadow-cyan-500/50 ">7</button>
-                <button className="rounded-md bg-gradient-to-br from-blue-400 from-10% to-indigo-800 to-90% shadow-lg hover:shadow-cyan-500/50 ">8</button>
-                <button className="rounded-md bg-gradient-to-br from-blue-400 from-10% to-indigo-800 to-90% shadow-lg hover:shadow-cyan-500/50 ">9</button>
-                <button className="rounded-md bg-gradient-to-br from-blue-400 from-10% to-indigo-800 to-90% shadow-lg hover:shadow-cyan-500/50 ">+/-</button>
-                <button className="rounded-md bg-gradient-to-br from-blue-400 from-10% to-indigo-800 to-90% shadow-lg hover:shadow-cyan-500/50 ">0</button>
-                <button className="rounded-md bg-gradient-to-br from-blue-400 from-10% to-indigo-800 to-90% shadow-lg hover:shadow-cyan-500/50 ">.</button>
+                <Btn type="degrad" text="1"></Btn>
+                <Btn type="degrad" text="2"></Btn>
+                <Btn type="degrad" text="3"></Btn>
+                <Btn type="degrad" text="4"></Btn>
+                <Btn type="degrad" text="5"></Btn>
+                <Btn type="degrad" text="6"></Btn>
+                <Btn type="degrad" text="7"></Btn>
+                <Btn type="degrad" text="8"></Btn>
+                <Btn type="degrad" text="9"></Btn>
+                <Btn type="degrad" text="+/-"></Btn>
+                <Btn type="degrad" text="0"></Btn>
+                <Btn type="degrad" text="."></Btn>
               </div>
               <div className="rounded-md bg-stone-900 col-span-2 grid gap-2 ">
-                <button className="rounded-md bg-slate-600 shadow-lg hover:shadow-cyan-500/50 ">+</button>
-                <button className="rounded-md bg-slate-600 shadow-lg hover:shadow-cyan-500/50 ">-</button>
-                <button className="rounded-md bg-slate-600 shadow-lg hover:shadow-cyan-500/50 ">*</button>
-                <button className="rounded-md bg-slate-600 shadow-lg hover:shadow-cyan-500/50 ">/</button>
-                <button className="rounded-md bg-slate-600 shadow-lg hover:shadow-cyan-500/50 ">sin</button>
-                <button className="rounded-md bg-slate-600 shadow-lg hover:shadow-cyan-500/50 ">cos</button>
-                <button className="rounded-md bg-slate-600 shadow-lg hover:shadow-cyan-500/50 col-span-2">=</button>
+                <Btn text="+"></Btn>
+                <Btn text="-"></Btn>
+                <Btn text="*"></Btn>
+                <Btn text="/"></Btn>
+                <Btn text="sin"></Btn>
+                <Btn text="cos"></Btn>
+                <Btn class="col-span-2" text="="></Btn>
               </div>
             </div>
           </div>
--- src/components/Btn.tsx
+++ src/components/Btn.tsx
@@ -1,6 +1,20 @@
-export default function Btn(props: { text: string }) {
+import { useEffect, useState } from "react"
+
+export default function Btn(props: { text: string, type?: string, class?: string }) {
+    const [className, setClassName] = useState('')
+    useEffect(() => {
+        switch (props.type) {
+            case 'degrad':
+                setClassName('rounded-md bg-gradient-to-br from-blue-400 from-10% to-indigo-800 to-90% shadow-lg hover:shadow-cyan-500/50')
+                break
+            default:
+                setClassName('rounded-md bg-slate-600 shadow-lg hover:shadow-cyan-500/50	text-white')
+        }
+    }, [])
+
     return <>
-        <button className="basis-1/5 rounded-md bg-slate-600 shadow-lg hover:shadow-cyan-500/50	text-white">
+        { }
+        <button className={`${className} ${props.class}`}>
             {props.text}
         </button>
     </>
