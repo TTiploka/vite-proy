485b850d7b765547e5ce00113b58e929c2ee9d2f
Dricc-new <diegoramoncc17@gmail.com>
2023-10-03 17:59:10 -0400

    children btn component add

--- src/App.tsx
+++ src/App.tsx
@@ -12,35 +12,35 @@ function App() {
             <input type="text" className="border rounded-md p-4 w-full bg-stone-900 text-white m-0 bottom-2  text-4xl" />
 
             <div className="flex flex-row gap-2 ">
-              <Btn class='basis-1/5' text="PI"></Btn>
-              <Btn class='basis-1/5' text="e"></Btn>
-              <Btn class='basis-1/5' text="|n|"></Btn>
-              <Btn class='basis-1/5' text="C"></Btn>
+              <Btn class='basis-1/5'>PI</Btn>
+              <Btn class='basis-1/5'>e</Btn>
+              <Btn class='basis-1/5'>|n|</Btn>
+              <Btn class='basis-1/5'>C</Btn>
               <button className="basis-1/5 rounded-md bg-slate-600 shadow-lg hover:shadow-cyan-500/50	text-white"><button className="bg-slate-950 w-1/3  border border-white rounded rounded-l-full ">X</button></button>
             </div>
             <div className="grid grid-cols-5 gap-2 row-span-4">
               <div className="rounded-md bg-stone-900 col-span-3 grid grid-cols-3 gap-2 ">
-                <Btn type="degrad" text="1"></Btn>
-                <Btn type="degrad" text="2"></Btn>
-                <Btn type="degrad" text="3"></Btn>
-                <Btn type="degrad" text="4"></Btn>
-                <Btn type="degrad" text="5"></Btn>
-                <Btn type="degrad" text="6"></Btn>
-                <Btn type="degrad" text="7"></Btn>
-                <Btn type="degrad" text="8"></Btn>
-                <Btn type="degrad" text="9"></Btn>
-                <Btn type="degrad" text="+/-"></Btn>
-                <Btn type="degrad" text="0"></Btn>
-                <Btn type="degrad" text="."></Btn>
+                <Btn type="degrad">1</Btn>
+                <Btn type="degrad">2</Btn>
+                <Btn type="degrad">3</Btn>
+                <Btn type="degrad">4</Btn>
+                <Btn type="degrad">5</Btn>
+                <Btn type="degrad">6</Btn>
+                <Btn type="degrad">7</Btn>
+                <Btn type="degrad">8</Btn>
+                <Btn type="degrad">9</Btn>
+                <Btn type="degrad">+/-</Btn>
+                <Btn type="degrad">0</Btn>
+                <Btn type="degrad">.</Btn>
               </div>
               <div className="rounded-md bg-stone-900 col-span-2 grid gap-2 ">
-                <Btn text="+"></Btn>
-                <Btn text="-"></Btn>
-                <Btn text="*"></Btn>
-                <Btn text="/"></Btn>
-                <Btn text="sin"></Btn>
-                <Btn text="cos"></Btn>
-                <Btn class="col-span-2" text="="></Btn>
+                <Btn>+</Btn>
+                <Btn>-</Btn>
+                <Btn>*</Btn>
+                <Btn>/</Btn>
+                <Btn>sin</Btn>
+                <Btn>cos</Btn>
+                <Btn class="col-span-2">=</Btn>
               </div>
             </div>
           </div>
--- src/components/Btn.tsx
+++ src/components/Btn.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react"
 
-export default function Btn(props: { text: string, type?: string, class?: string }) {
+export default function Btn(props: { children: string, type?: string, class?: string }) {
     const [className, setClassName] = useState('')
     useEffect(() => {
+        setTimeout
         switch (props.type) {
             case 'degrad':
                 setClassName('rounded-md bg-gradient-to-br from-blue-400 from-10% to-indigo-800 to-90% shadow-lg hover:shadow-cyan-500/50')
@@ -15,7 +16,7 @@ export default function Btn(props: { text: string, type?: string, class?: string
     return <>
         { }
         <button className={`${className} ${props.class}`}>
-            {props.text}
+            {props.children}
         </button>
     </>
 }
